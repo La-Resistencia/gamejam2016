811f722330bce6962ae10dc4da0bb26d7f365b66
Jose Pfuturi <uchiha.futaro@gmail.com>
2016-01-30 19:10:44 -0500

    test multiplayer scene

---
+++ unity-project/Assets/MultiplayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MultiplayerController : MonoBehaviour {
+
+    private bool isInRequest = false;
+    private WWW www = null;
+
+	// Use this for initialization
+	void Start () {
+        www = new WWW("http://ws.gamejam2016.laresistencia.pe/ping");
+
+        StartCoroutine(handleWWW(www));
+	}
+
+    IEnumerator handleWWW(WWW _www)
+    {
+        yield return _www;
+
+        Debug.Log("returned " + _www.text);
+        StartCoroutine(handleWWW(www));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
